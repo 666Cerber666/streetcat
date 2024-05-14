98057afab9dcb497d41d84a6f21a7acb4f31225b
666Cerber666 <forgikiname@mail.ru>
2024-05-15 00:08:15 +0400

    update pageInstrutions

--- src/pages/PageInstructions.vue
+++ src/pages/PageInstructions.vue
@@ -1,27 +1,42 @@
 <template>
     <q-layout class="wrapper">
       <div class="flex w-full flex-col items-center w-full mt-3">
-        <div class="flex">
-          <div></div>
-          <div></div>
+        <div class="flex items-center w-full justify-evenly">
+          <div class="wrapper_range"><input type="range" min="0" max="100" value="50" step="12.5" /></div>
+          <div class="circle">
+            <div class="inner-circle"></div>
+            <div class="circle-quarter quarter-1"></div>
+            <div class="circle-quarter quarter-2"></div>
+            <div class="circle-quarter quarter-3"></div>
+            <div class="circle-quarter quarter-4"></div>
+          <!-- Текст в каждом углу -->
+            <span class="text text-1">A</span>
+            <span class="text text-2">B</span>
+            <span class="text text-3">C</span>
+            <span class="text text-4">D</span>
+        </div>
           <div><button class="button" @click="toggleAnimation"></button></div> <!-- Кнопка старт/пауза -->
         </div>
-        <div class="absolute flex bottom-1">
+      </div>
+    </q-layout>
+    <div class="absolute flex bottom-0 inset-x-0 bg-block h-12 w-screen items-center justify-evenly">
           <div class="volume-container">
             <!-- Иконка кнопки регулировки громкости -->
             <button @click="toggleVolume" class="volume-icon-button">
-              <i class="fas fa-volume-up"></i> <!-- Пример иконки из Font Awesome -->
+              <i class="fas fa-volume-up"></i>
             </button>
             <!-- Вертикальный range с регулировкой громкости -->
             <transition name="slide-fade">
               <input v-if="showVolumeSlider" type="range" orient="vertical" class="volume-slider" min="0" max="100" v-model="volume" @input="adjustVolume" key="volume-slider">
             </transition>
           </div>
-          <div><button @click="chooseMusic"></button> </div> <!-- Кнопка выбора музыки -->
-          <div><button @click="toggleEqualizer"></button> </div> <!-- Кнопка эквалайзера -->
+          <div><button class="bg-aqua rounded-full w-10 h-10" @click="chooseMusic"></button> </div> <!-- Кнопка выбора музыки -->
+          <div>
+            <button @click="toggleEqualizer" class="equalizer-icon-button w-10 h-10">
+                <i class="fas fa-bullseye"></i> 
+              </button>
+          </div> 
         </div>
-      </div>
-    </q-layout>
   </template>
   
   <script>
@@ -56,9 +71,12 @@
     }
   }
   </script>
+
+  <style lang="scss">
+    @import 'range.scss'
+  </style>
   
   <style scoped>
-/* Стили для кнопки и ее анимации */
 .button {
   border: 0;
   background: transparent;
@@ -115,4 +133,79 @@
     transition: all 0.3s ease-out;
   transform: translateY(0); /* Начальное положение (скрытый снизу) */
 }
+
+.circle {
+    position: relative;
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    background-color: lightgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .inner-circle {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: white;
+    border-radius: 50%;
+  }
+
+  .circle-quarter {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    background-color: transparent;
+    border-radius: 50%;
+  }
+
+  .quarter-1 {
+    top: 0;
+    left: 0;
+  }
+
+  .quarter-2 {
+    top: 0;
+    right: 0;
+  }
+
+  .quarter-3 {
+    bottom: 0;
+    left: 0;
+  }
+
+  .quarter-4 {
+    bottom: 0;
+    right: 0;
+  }
+
+  .text {
+    position: absolute;
+    font-size: 36px;
+    color: black;
+  }
+
+  /* Указываем позиции текста */
+  .text-1 {
+    top: 5px;
+    left: 5px;
+  }
+
+  .text-2 {
+    top: 5px;
+    right: 5px;
+  }
+
+  .text-3 {
+    bottom: 5px;
+    left: 5px;
+  }
+
+  .text-4 {
+    bottom: 5px;
+    right: 5px;
+  }
+
 </style>
