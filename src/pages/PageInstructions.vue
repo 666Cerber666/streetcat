0463716b78b8b0bce322a4cfa5fdd5d9c205ae10
666Cerber666 <forgikiname@mail.ru>
2024-05-17 00:01:20 +0400

    update page pageinstructions

--- src/pages/PageInstructions.vue
+++ src/pages/PageInstructions.vue
@@ -1,12 +1,28 @@
 <template>
   <q-layout class="wrapper">
-    <div class="flex w-full flex-col items-center justify-center w-full h-screen">
-      <div class="flex items-center w-full justify-evenly">
-        <div class="wrapper_range">
-          <input type="range" class="volume-rangers" v-model="intervalValue" min="0" max="10000" default="5000" step="1000" />
+    <div class="flex w-full flex-col items-end justify-center h-full gap-3 px-3">
+      <q-btn class="w-12 h-12 mt-2 mr-2 bg-block float-right rounded-xl text-center flex items-center justify-center">
+        <span class="close" @click="goToHomePage">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+        </svg>
+      </span>
+      </q-btn>
+      <div class="flex items-center w-full bg-block block-plates justify-evenly rounded-xl py-12 mb-2">
+        <div class="wrapper_range" ref="rangeWrapper">
+          <q-slider
+            v-model="intervalValue"
+            :min="0"
+            :max="10000"
+            :step="1000"
+            vertical
+            label
+            reverse
+            class="volume-rangers"
+          />
         </div>
         <div>
-          <div class="circle" ref="circle">
+          <div class="circle bg-aqua" ref="circle">
             <div class="inner-circle text-black" ref="innerCircle"></div>
             <div class="circle-quarter quarter-1"></div>
             <div class="circle-quarter quarter-2"></div>
@@ -19,9 +35,12 @@
             <span class="text text-4">D</span>
           </div>
             <div class="mt-20">
-              <div class="flex flex-col items-center mt-3" v-for="(value, index) in displayedValues" :key="index">
-              {{ value }}
-            </div>
+              <div class="flex flex-col items-center mt-3 arrayValues" v-for="(value, index) in displayedValues" :key="index">
+                {{ value }}
+              </div>
+              <transition-group name="fade">
+                <div v-if="newItem" class="arrayValues">{{ newItem }}</div>
+              </transition-group>
           </div>
         </div>
         <div>
@@ -30,7 +49,7 @@
       </div>
     </div>
   </q-layout>
-  <div class="absolute flex bottom-0 inset-x-0 bg-block h-12 w-screen items-center justify-evenly">
+  <div class="absolute flex bottom-0 inset-x-0 bg-block h-12 w-screen items-center justify-evenly mobile-only">
     <div class="volume-container">
       <!-- Иконка кнопки регулировки громкости -->
       <button @click="toggleVolume" class="volume-icon-button">
@@ -38,14 +57,48 @@
       </button>
       <!-- Вертикальный range с регулировкой громкости -->
       <transition name="slide-fade">
-        <input v-if="showVolumeSlider" type="range" class="volume-slider" min="0" max="100" v-model="volume" @input="adjustVolume" key="volume-slider">
+        <div v-if="showVolumeSlider" class="volume-slider-container">
+          <q-slider
+            v-model="volume"
+            @input="adjustVolume"
+            :min="0"
+            :max="100"
+            vertical
+            label
+            reverse
+            class="volume-slider"
+          />
+        </div>
       </transition>
     </div>
     <div>
-      <button class="bg-aqua rounded-full w-10 h-10 mt-1" @click="chooseMusic"></button>
+      <button class="rounded-full w-10 h-10 mb-1 plate" @click="chooseMusic" :class="{ 'rotate-animation': isRotating }"></button>
     </div> <!-- Кнопка выбора музыки -->
     <div>
       <button @click="toggleEqualizer" class="equalizer-icon-button w-10 h-10">
+        <i class="fas fa-bullseye"></i>
+      </button>
+    </div>
+  </div>
+
+  <div class="absolute flex bottom-0 inset-x-0 bg-block h-24 gap-2 w-screen items-center justify-evenly desktop-only">
+      <!-- Вертикальный range с регулировкой громкости -->
+      <div class="flex items-center gap-2">
+          <i class="fas fa-volume-up"></i>
+          <q-slider
+            v-model="volume"
+            @input="adjustVolume"
+            :min="0"
+            :max="100"
+            label
+            class="w-36"
+          />
+      </div>
+    <div>
+      <button class="absolute bottom-0 rounded-full w-20 h-20 mb-1 plate" @click="chooseMusic" :class="{ 'rotate-animation': isRotating }"></button>
+    </div> <!-- Кнопка выбора музыки -->
+    <div>
+      <button @click="toggleEqualizer" class="equalizer-icon-button w-20 h-20">
         <i class="fas fa-bullseye"></i>
       </button>
     </div>
@@ -56,12 +109,16 @@
 import { ref, watch, onMounted, computed } from 'vue';
 
 const circle = ref(null);
+const rangeWrapper = ref(null);
 const innerCircle = ref(null);
 const values = ref(["A1", "B2", "C3", "D4", "A11", "B22", "C33", "D44"]);
 let currentIndex = 0;
 let intervalId = null;
 const isRunning = ref(false);
 const intervalValue = ref(5000); // Значение по умолчанию 5000 мс
+const showVolumeSlider = ref(false);
+const isRotating = ref(false);
+const newItem = ref(null);
 
 const startUpdate = () => {
   if (!isRunning.value) {
@@ -69,6 +126,10 @@ const startUpdate = () => {
     intervalId = setInterval(updateValues, intervalValue.value);
   }
 };
+
+const toggleRotation = () => {
+  isRotating.value = !isRotating.value; // Инвертирование состояния анимации при каждом нажатии на кнопку
+}
 
 const updateValues = () => {
   // Обновление значений внутри круга
@@ -107,8 +168,6 @@ const updateValues = () => {
 const pauseUpdate = () => {
   isRunning.value = false;
   clearInterval(intervalId);
-  const btn = document.querySelector(".button");
-  btn.classList.remove("paused");
 };
 
 const defaultValues = () => {
@@ -118,12 +177,20 @@ const defaultValues = () => {
 const toggleAnimation = () => {
   if (isRunning.value) {
     pauseUpdate();
+    const btn = document.querySelector(".button");
+    btn.classList.toggle("paused");
+    toggleRotation();
   } else {
     startUpdate();
+    const btn = document.querySelector(".button");
+    btn.classList.toggle("paused");
+    toggleRotation();
   }
-  const btn = document.querySelector(".button");
-  btn.classList.toggle("paused");
 };
+
+const toggleVolume = () => {
+  showVolumeSlider.value = !showVolumeSlider.value;
+}
 
 watch(intervalValue, (newValue) => {
   pauseUpdate();
@@ -139,17 +206,19 @@ const showNextValues = () => {
   }
 };
 
+const addNewValue = (newValue) => {
+  newItem.value = newValue;
+  setTimeout(() => {
+    displayedValues.value.push(newValue);
+    newItem.value = null;
+  }, 300); // Задержка должна быть равной продолжительности анимации
+};
+
 onMounted(() => {
   defaultValues(); // Установка значений по умолчанию
 });
 
 </script>
-
-
-
-<style scoped lang="scss">
-  @import 'range.scss'
-</style>
   
 <style scoped>
 .button {
@@ -174,47 +243,66 @@ onMounted(() => {
   border-color: transparent transparent transparent #404040;
 }
 
-/* Стили для иконки кнопки регулировки громкости и вертикального range */
-.volume-container {
-  position: relative; /* Добавлено */
-  display: inline-block;
-  margin-left: 20px; /* Расстояние между кнопкой и вертикальным range */
-}
-
 .volume-icon-button {
   border: none;
   background: none;
   cursor: pointer;
+  width:50px;
 }
 
-.volume-slider {
+/* Стили для контейнера слайдера */
+.volume-slider-container {
   position: absolute;
-  left: 50%; /* Центрирование range относительно иконки */
+  left: calc(100% + (50 / 5)); /* Центрирование контейнера относительно иконки */
   bottom: 0; /* Начальное положение сверху */
-  transform: translateX(-50%); /* Центрирование range относительно иконки */
+  transform: translate(0%, -50%); /* Перемещение контейнера вверх на высоту слайдера */
+}
+
+/* Стили для самого слайдера */
+.volume-slider {
   width: 20px;
   height: 150px;
-  margin-bottom:50px;
   -webkit-appearance: slider-vertical;
   appearance: slider-vertical;
 }
 
+/* Анимация */
 .slide-fade-enter-active {
   transition: all 0.3s ease-out;
-  transform: translateY(-100%); /* Конечное положение (выезжание сверху вниз) */
+  transform: translateY(-50%);
 }
 
 .slide-fade-enter-from {
-    transition: all 0.3s ease-out;
-  transform: translateY(0); /* Начальное положение (скрытый снизу) */
+  transition: all 0.3s ease-out;
+  transform: translateY(0);
+}
+
+.plate{
+  background-image: url('../assets/images/plate.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  left: 47%
+}
+
+.rotate-animation {
+  animation: rotate 10s linear infinite;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 
 .circle {
     position: relative;
-    width: 200px;
-    height: 200px;
+    width: 300px;
+    height: 300px;
     border-radius: 50%;
-    background-color: lightgray;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -235,8 +323,8 @@ onMounted(() => {
 
   .circle-quarter {
     position: absolute;
-    width: 100px;
-    height: 100px;
+    width: 150px;
+    height: 150px;
     background-color: transparent;
   }
 
@@ -270,8 +358,8 @@ onMounted(() => {
 
   .text {
     position: absolute;
-    font-size: 24px;
-    color: black;
+    font-size: 36px;
+    color: white;
   }
 
   /* Указываем позиции текста */
@@ -298,5 +386,22 @@ onMounted(() => {
   .active { background-color: black; transition: ease-in-out 0.3s}
 
   .activetext { color: rgb(16, 17, 109); font-size: 28px; transition: ease-in-out 0.3s}
+
+  .arrayValues{
+    font-size:24px;
+    color:#ABD5D6;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active в более новых версиях Vue */ {
+    opacity: 0;
+  }
+
+  .block-plates{
+    min-height: 560px;
+  }
 
 </style>
